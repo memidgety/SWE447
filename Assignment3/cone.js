03921811c6fc2519b0eb15038190f4442031d700
Norman <norman_pham@yahoo.com>
2017-02-06 18:44:53 -0800

    Added Assignment3 and  more common .js

---
+++ Assignment3/cone.js
@@ -0,0 +1,25 @@
+var gl = null;
+var cone = null;
+
+function init() {
+    var canvas = document.getElementById( "webgl-canvas" );
+	cone = new Cone( n );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+
+    if ( !gl ) {
+        alert("Unable to setup WebGL");
+        return;
+    }
+
+    gl.clearColor( 1.0, 0.0, 0.0, 1.0 );
+
+    //render();
+	cone.render();
+}
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+}
+
+window.onload = init;
